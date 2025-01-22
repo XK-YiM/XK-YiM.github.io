2f4e30895d1231dd05156c0e9102be4b19b3ece8
XK-YiM <XK_YiM@outlook.com>
2025-01-22 19:25:22 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <title>YiM | 首页</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+    <div class="box">
+        <div id="menu"> <!-- 菜单栏 -->
+            <ul>
+                <li><a href="home.html">首页</a></li>
+                <li><a href="material.html">物资</a></li>
+                <li><a href="join.html">加入我们</a></li>
+                <li><a href="about.html">关于我们</a></li>
+            </ul>
+        </div>
+        <div class="content">
+            <h1>欢迎来到YiM</h1>
+            <h2>YiM —— 用方块创造无限可能</h2>
+            <p>生是方块人，死是方块魂。</p>
+            <p>方块是我们创造的，方块是我们所爱。用方块，创造无限可能。</p>
+        </div>
+        <footer>
+            <p>© 2025 YiM By 日月YiM. 版权所有.</p>
+        </footer>
+    </div>
+</body>
+</html>
